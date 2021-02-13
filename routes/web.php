db21b91a75be20215c8c3c3334048fe823fc78fd
Niti <niti.pimon@hotmail.com>
2021-02-13 14:49:26 +0700

    add search page and restaurant page.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/search', function () {
+    return view('search');
+})->name('search');
+Route::middleware(['auth:sanctum', 'verified'])->get('/restaurant', function () {
+    return view('restaurant');
+})->name('restaurant');
