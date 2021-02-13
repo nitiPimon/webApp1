756a5f7da408fe3ed24d1cc10959787688b25b49
Niti <niti.pimon@hotmail.com>
2021-02-13 15:05:07 +0700

    Responsive Navigation Menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -10,10 +10,16 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                <!--**** Navigation Links ****-->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('search') }}" :active="request()->routeIs('search')">
+                        {{ __('Search') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('restaurant') }}" :active="request()->routeIs('restaurant')">
+                        {{ __('Restaurant') }}
                     </x-jet-nav-link>
                 </div>
             </div>
@@ -135,12 +141,18 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- ****Responsive Navigation Menu**** -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+            <x-jet-nav-link href="{{ route('search') }}" :active="request()->routeIs('search')">
+                {{ __('Search') }}
+            </x-jet-nav-link>
+            <x-jet-nav-link href="{{ route('restaurant') }}" :active="request()->routeIs('restaurant')">
+                {{ __('Restaurant') }}
+            </x-jet-nav-link>            
         </div>
 
         <!-- Responsive Settings Options -->
